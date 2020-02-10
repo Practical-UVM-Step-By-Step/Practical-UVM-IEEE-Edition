bfc3ebb9fe0952e4c560ab736bbbe050ce933959
admin <srivatsa9@gmail.com>
2020-02-10 12:25:31 -0800

    removed empty line

--- IEEE_version/UVM_Building_Blocks/generic/src/wb_master_seqr.sv
+++ IEEE_version/UVM_Building_Blocks/generic/src/wb_master_seqr.sv
@@ -3,7 +3,6 @@
     typedef class wb_transaction;
     typedef class wb_config;
     class wb_master_seqr extends uvm_sequencer # (wb_transaction);
-
         wb_config cfg;
 
         `uvm_component_utils(wb_master_seqr)
